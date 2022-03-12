3d35aca710189c940e7b0890cf94654a1f632e9e
Tamirat Tadesse <tadessetamirat170@gmail.com>
2022-03-12 15:14:23 +0300

    remove

--- src/Helper/SmsHelper.php
+++ src/Helper/SmsHelper.php
@@ -24,27 +24,27 @@ class SmsHelper {
            
     //  dd(1);
 
-        $response = $this->client->request(
-            'POST',
-            sprintf('%s/message-request/create',$this->container->getParameter("msg_api_host")),
-            [
-                'headers' => [
-                    'Accept' => 'application/json',
-                    'X-AUTH-TOKEN' => $this->container->getParameter("msg_api_token"),
-                ],
+    //     $response = $this->client->request(
+    //         'POST',
+    //         sprintf('%s/message-request/create',$this->container->getParameter("msg_api_host")),
+    //         [
+    //             'headers' => [
+    //                 'Accept' => 'application/json',
+    //                 'X-AUTH-TOKEN' => $this->container->getParameter("msg_api_token"),
+    //             ],
 
-              'body' => ['title' => $title, 'message'=>$message,"phone_numbers"=>$phone_numbers],
-            ],
-        );
+    //           'body' => ['title' => $title, 'message'=>$message,"phone_numbers"=>$phone_numbers],
+    //         ],
+    //     );
 
-        $statusCode = $response->getStatusCode();
-        // $content = $response->getContent();
-        // $content = $response->toArray();
+    //     $statusCode = $response->getStatusCode();
+    //     // $content = $response->getContent();
+    //     // $content = $response->toArray();
 
-        //  dd($response->getContent()
-        // );
-       return $statusCode==200;
-       return false;
+    //     //  dd($response->getContent()
+    //     // );
+    //    return $statusCode==200;
+    //    return false;
 
     } catch (\Throwable $th) {
        
